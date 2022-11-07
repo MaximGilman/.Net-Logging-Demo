4d452a7b68a04be177820fecbdc3545ff7f1f0f8
Gilman M <gilman.mm@parmalogica.ru>
2022-11-07 18:32:04 +0500

    Добавление опций для k6

--- k6-api-tests/script.js
+++ k6-api-tests/script.js
@@ -1,7 +1,15 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
 
-const API_URL = 'https://localhost:7151/Index';
+const API_URL = "https://localhost:7151/Index";
+export const options = {
+  stages: [
+    { duration: "30s", target: 10 },
+    { duration: "30s", target: 20 },
+    { duration: "30s", target: 100 },
+    { duration: "10s", target: 0 },
+  ],
+};
 
 export default function () {
   http.get(API_URL);
